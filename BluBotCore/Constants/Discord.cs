b626223b57f48fe81c6d648a679652d4ccf9c5a1
Mahsaap <vimhatt@hotmail.com>
2018-06-16 16:32:06 -0300

    Added Discord constants

---
+++ BluBotCore/Constants/Discord.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BluBotCore.Constants
+{
+    public class Discord
+    {
+        public static string Mahsaap { get { return "88798728948809728"; }}
+        public static string Space { get { return "101614418915266560"; }}
+    }
+}
