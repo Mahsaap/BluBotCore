7443071fd9c795976b8b92c24c7ef89a9f2325ae
Mahsaap <vimhatt@hotmail.com>
2020-04-29 01:35:14 -0300

    Add Debug channels to use for errors messages.

--- BluBotCore/Constants/DiscordIDs.cs
+++ BluBotCore/Constants/DiscordIDs.cs
@@ -4,5 +4,7 @@
     {
         public static ulong Mahsaap { get { return 88798728948809728; }}
         public static ulong Space { get { return 101614418915266560; }}
+        public static ulong DebugLogsWYK { get { return 402997860318314526; } }
+        public static ulong DebugLogsTest { get { return 446695777545224192; } }
     }
 }
