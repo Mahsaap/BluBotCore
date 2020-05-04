b89795950a8309fa38088171bfda48ff01834f78
tim.hatt <tim.hatt@innovasea.com>
2020-05-04 12:22:46 -0300

    Add BuildType for OBG/WYK Variants

--- BluBotCore/Constants/Version.cs
+++ BluBotCore/Constants/Version.cs
@@ -1,8 +1,11 @@
-﻿namespace BluBotCore
+﻿using BluBotCore.Global;
+
+namespace BluBotCore
 {
     public class Version
     {
         public static string Major { get { return "1"; } }
         public static string Minor { get { return "15"; } }
+        public static BuildType Build { get { return BuildType.OBG; } }
     }
 }
