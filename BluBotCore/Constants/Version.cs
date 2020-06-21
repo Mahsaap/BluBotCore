be84ceb3ebb81b4eabb35e59f21aa27e0af03d56
Mahsaap <vimhatt@hotmail.com>
2020-06-21 02:05:47 -0300

    Update Version 1.18

--- BluBotCore/Constants/Version.cs
+++ BluBotCore/Constants/Version.cs
@@ -5,7 +5,7 @@ namespace BluBotCore
     public class Version
     {
         public static string Major { get { return "1"; } }
-        public static string Minor { get { return "17"; } }
+        public static string Minor { get { return "18"; } }
         public static string Build { get { return BuildType.WYK.Value; } }
     }
 }
